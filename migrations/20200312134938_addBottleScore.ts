699595be13cf801f1b5fa2e479491de4a82c112a
Maxime GUIHAL <sgp.jaconil@gmail.com>
2020-03-12 14:21:35 +0100

    feat: Add bottles score db migration

---
+++ migrations/20200312134938_addBottleScore.ts
@@ -0,0 +1,22 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.table('game', table => {
+    table.integer('score').alter();
+    table.integer('scoreLeftBottle');
+    table.integer('scoreMiddleBottle');
+    table.integer('scoreRightBottle');
+    table.integer('scoreMalusBottle');
+  });
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.table('game', table => {
+    table.dropColumn('scoreLeftBottle');
+    table.dropColumn('scoreMiddleBottle');
+    table.dropColumn('scoreRightBottle');
+    table.dropColumn('scoreMalusBottle');
+    table.integer('score').notNullable().alter();
+  });
+}
